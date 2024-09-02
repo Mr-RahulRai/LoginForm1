c172ea0f542157c3c66d82cba56c03eac7714272
Mr-RahulRai <rahulrai17101998@gmail.com>
2024-09-03 02:54:12 +0530

    Add files via upload

---
+++ loginScript.js
@@ -0,0 +1,13 @@
+const title = document.getElementById('title');
+const namefield = document.getElementById('nameField');
+const signinbtn =document.getElementById('signinbtn');
+const signupbtn= document.getElementById('signupbtn');
+
+signupbtn.onclick = function(){
+    title.innerHTML ="Sign Up";
+    nameField.style.display = "block";
+}
+signinbtn.onclick = function(){
+    title.innerHTML ="Login";
+    nameField.style.display = "none";
+}
